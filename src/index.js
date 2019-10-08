c364bc026b0ee79d9bab446fc2e6677cf8e86a7f
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-10-07 23:49:22 -0400

    Created the keyboard component
    Created the keyboard component to start the challenge

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Keypad from "./keypad";
 import "./styles.css";
 
-function App() {
+function Index() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="Index">
+      <Keypad />
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Index />, rootElement);
---
+++ src/keypad.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+var Keypad = () => {
+  return (
+    <div>
+      <div>
+        {" "}
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+
+      <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </div>
+
+      <div>
+        {" "}
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
+    </div>
+  );
+};
+
+export default Keypad;
