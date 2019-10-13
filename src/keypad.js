a96573691e3feb9e077a0144b6dc6c79d3804974
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-10-13 13:10:10 -0400

    Finished Up the code checker
    The code checker now congratulates users when they get the code right, tells them when they get it wrong, and gives them the ability to reset what they had wrote

--- src/keypad.js
+++ src/keypad.js
@@ -1,27 +1,83 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+//Password is TwoFourSixEight, Two + Four + Six + Eight
+
+var Reducer = (state, action) => {
+  if (
+    state === "Wrong Code Please Try Again, Click any key to reset!" ||
+    state ===
+      "Congratulations, the passcode is correct, Press Any key to reset it!"
+  ) {
+    return "";
+  }
+  if (
+    (state === "" && action === "2") ||
+    (state === "2" && action === "4") ||
+    (state === "24" && action === "6") ||
+    (state === "246" && action === "8")
+  ) {
+    switch (action) {
+      case "1":
+        return state + "1";
+      case "2":
+        return state + "2";
+      case "3":
+        return state + "3";
+
+      case "4":
+        return state + "4";
+      case "5":
+        return state + "5";
+      case "6":
+        return state + "6";
+
+      case "7":
+        return state + "7";
+      case "8":
+        if (state === "246" && action === "8") {
+          return "Congratulations, the passcode is correct, Press Any key to reset it!";
+        } else {
+          return state + "8";
+        }
+
+      case "9":
+        return state + "9";
+
+      default:
+        return state;
+    }
+  } else {
+    return "Wrong Code Please Try Again, Click any key to reset!";
+  }
+};
 
 var Keypad = () => {
+  var [state, dispatch] = useReducer(Reducer, "");
+
+  //If state is on the right track keep going
+
   return (
     <div>
       <div>
         {" "}
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
+        <button onClick={() => dispatch("1")}>1</button>
+        <button onClick={() => dispatch("2")}>2</button>
+        <button onClick={() => dispatch("3")}>3</button>
       </div>
 
       <div>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <button onClick={() => dispatch("4")}>4</button>
+        <button onClick={() => dispatch("5")}>5</button>
+        <button onClick={() => dispatch("6")}>6</button>
       </div>
 
       <div>
         {" "}
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <button onClick={() => dispatch("7")}>7</button>
+        <button onClick={() => dispatch("8")}>8</button>
+        <button onClick={() => dispatch("9")}>9</button>
       </div>
+      <div>{state}</div>
     </div>
   );
 };
